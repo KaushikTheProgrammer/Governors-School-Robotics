892b96d68aacc51b90dd82809a09813f39ea60d4
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-06 13:47:59 -0400

    More color sensor

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 -u
 from ev3dev.ev3 import *
 from time import sleep
 
 colorSensor = ColorSensor()
 colorSensor.mode = 'COL-REFLECT'
-val = colorSensor.value()
-print(val)
+
+while True:
+    val = colorSensor.value()
+    print(val)
