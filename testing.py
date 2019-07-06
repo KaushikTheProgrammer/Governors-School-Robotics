c1ecdd56f2a8be67f7328c31266b74d13f6e92ee
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-06 13:45:28 -0400

    Color Sensor testing

--- testing.py
+++ testing.py
@@ -2,14 +2,7 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-right = LargeMotor('outA')
-left = LargeMotor('outD')
-
-for x in range(100, 900):
-    right.run_forever(speed_sp=x)
-    left.run_forever(speed_sp=-x)
-    sleep(0.05)
-sleep(1)
-
-right.stop(stop_action="hold")
-left.stop(stop_action="hold")
+colorSensor = ColorSensor()
+colorSensor.mode = 'COL-REFLECT'
+val = colorSensor.value()
+print(val)
