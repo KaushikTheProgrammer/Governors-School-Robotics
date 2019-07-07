826278ceae441b5f508e783c1d811f3c8200c18b
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-07 15:28:02 -0400

    Initial Line Following

--- testing.py
+++ testing.py
@@ -5,6 +5,23 @@ from time import sleep
 rightLight = LightSensor('in1')
 rightLight.mode = 'REFLECT'
 
+rightMotor = LargeMotor('outD')
+leftMotor = LargeMotor('outA')
+
 while True:
     right = rightLight.reflected_light_intensity
-    print("right", right)
+    # Black
+    if right < 30:
+        # Turn Left
+        rightMotor.run_forever(speed_sp=50)
+        leftMotor.run_forever(speed_sp=0)
+
+    elif right > 50:
+        # Turn Right
+        # Turn Left
+        rightMotor.run_forever(speed_sp=0)
+        leftMotor.run_forever(speed_sp=50)
+    else:
+        # Go Forward
+        rightMotor.run_forever(speed_sp=100)
+        leftMotor.run_forever(speed_sp=100)
