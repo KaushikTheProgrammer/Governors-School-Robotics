e5d8e4a72eeea22a62e4629adf0e87b03def33a4
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-07 15:07:10 -0400

    Changed to light sensor

--- testing.py
+++ testing.py
@@ -2,9 +2,13 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-lightSensor = LightSensor()
-lightSensor.mode = 'REFLECT'
+rightLight = LightSensor()
+rightLight.mode = 'REFLECT'
+
+leftLight = LightSensor()
+leftLight.mode = 'REFLECT'
 
 while True:
-    val = lightSensor.reflected_light_intensity
-    print(val)
+    right = rightLight.reflected_light_intensity
+    left = leftLight.reflected_light_intensity
+    print("right", right, "left", left)
