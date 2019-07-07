8f5af7ca65e31a408afc7f68118b603ba1b86133
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-07 17:07:27 -0400

    More Motor Testing

--- testing.py
+++ testing.py
@@ -14,7 +14,7 @@ while True:
     if right < 40:
         # Turn Left
         rightMotor.run_forever(speed_sp=-150)
-        leftMotor.run_forever(speed_sp=100)
+        leftMotor.run_forever(speed_sp=150)
 
     elif right > 50:
         # Turn Right
