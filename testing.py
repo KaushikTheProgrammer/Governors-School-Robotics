cd58c0adf61ac056f1ecaf6153df7cb3048bc2f8
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-10 21:03:11 -0400

    testing vals

--- testing.py
+++ testing.py
@@ -21,8 +21,8 @@ while True:
 
     print("light", lightOutput, "error", error, "leftMotor", leftMotorVal, "rightMotor", rightMotorVal)
 
-    # rightMotor.run_forever(speed_sp=-rightMotorVal)
-    # leftMotor.run_forever(speed_sp=-leftMotorVal)
+    rightMotor.run_forever(speed_sp=-rightMotorVal)
+    leftMotor.run_forever(speed_sp=-leftMotorVal)
     
 
 
