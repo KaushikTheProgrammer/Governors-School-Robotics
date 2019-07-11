45286ab5fd736832e48e0ff00e05abac5ce365f4
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-10 20:18:31 -0400

    testing vals

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@
 from ev3dev.ev3 import *
 
 centerLight = ColorSensor('in4')
-centerLight.MODE_COL_REFLECT = 'COL-REFLECT'
+centerLight.mode = 'COL-REFLECT'
 
 rightMotor = LargeMotor('outD')
 leftMotor = LargeMotor('outA')
