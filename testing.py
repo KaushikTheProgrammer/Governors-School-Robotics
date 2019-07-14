dcd40a95546f76fa9986b6ae1a73cb074bb4d83a
KaushikTheProgrammer <kaushikpprakash@gmail.com>
2019-07-14 10:19:57 -0400

    testing

--- testing.py
+++ testing.py
@@ -77,7 +77,7 @@ while line:
 
 
 # At the start of the house
-while ultrasonicSensor.distance_centimeters > 2:
+while ultrasonicSensor.distance_centimeters > 3:
     print(ultrasonicSensor.distance_centimeters)
     rightMotor.run_forever(speed_sp=-200)
     leftMotor.run_forever(speed_sp=-200)
